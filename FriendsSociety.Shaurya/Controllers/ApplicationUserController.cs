4ad0ee9c0c2aa7bc60823cd1f70c5f9548fe769b
CORESVCS\Suraj.Balwani <suraj.balwani@civica.com>
2025-03-20 11:38:19 +0000

    Add project files.

---
+++ FriendsSociety.Shaurya/Controllers/ApplicationUserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FriendsSociety.Shaurya.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUserController : ControllerBase
+    {
+        [HttpGet]
+
+    }
+}
---
+++ FriendsSociety.Shaurya/Entities/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿namespace FriendsSociety.Shaurya.Entities
+{
+    public class ApplicationUser
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+    }
+}
