2a27da17701e2657abe36a722264ffbc6b86324c
Suraj Balwani <surajbalwani.itm@gmail.com>
2025-04-05 02:30:44 +0530

    Added new table called participants

--- FriendsSociety.Shaurya/Data/DataContext.cs
+++ FriendsSociety.Shaurya/Data/DataContext.cs
@@ -11,5 +11,6 @@ namespace FriendsSociety.Shaurya.Data
         }
 
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Participants> Participants { get; set; }
     }
 }
